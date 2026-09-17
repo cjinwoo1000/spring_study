d04e4a14eb8cb70567bf747ae59044656055a67b
cjinwoo1000 <cjw1000123442@gmail.com>
2026-09-17 13:12:02 +0900

    Add files via upload

---
+++ spring_web/src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,20 @@
+//패키지 위치 설정
+package com.example.demo;
+
+//매핑도구
+import org.springframework.web.bind.annotation.GetMapping;
+//웹요청 처리도구
+import org.springframework.web.bind.annotation.RestController;
+
+//웹요청 처리
+@RestController
+//누구나 볼수있는 public틀
+public class HelloController {
+    // (/hello)로 들어 왔을때 요청을 처리
+    @GetMapping("/hello")
+
+    //실제 실행 함수
+    public String hello() {
+        return "안녕하세요! 스프링 첫 화면입니다.";
+    }
+}
